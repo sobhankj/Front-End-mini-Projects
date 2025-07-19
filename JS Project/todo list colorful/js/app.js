1412d06539c922d30ad989b50d7711af9f8ba005
Sobhan Kooshki <sobhankj.tehran@gmail.com>
2025-07-19 16:22:37 +0330

    add todo list with color background

---
+++ JS Project/todo list colorful/js/app.js
@@ -0,0 +1,81 @@
+const add_button = document.querySelector(".add_button");
+const close_button_task = document.querySelector(".close_button_task");
+const cancel_button_task = document.querySelector(".cancel_button_task");
+const add_button_task = document.querySelector(".add_button_task");
+const all_delete_button = document.querySelector(".all_delete_button");
+var single_delete_buttons = document.querySelectorAll(".single_delete_button");
+const color_items = document.querySelectorAll(".color_item");
+const add_task_input = document.querySelector(".add_task_input");
+
+color_items.forEach(color_item => {
+    color_item.style.backgroundColor = color_item.dataset.color;
+    color_item.addEventListener("click", () => {
+        add_task_input.style.borderColor = color_item.dataset.color;
+        const selected_color = document.querySelector(".selected_color");
+        selected_color.classList.remove("selected_color");
+        color_item.classList.add("selected_color");
+    })
+})
+
+
+add_button.addEventListener("click", () => {
+    document.body.classList.remove("hidden");
+    const todo_input = document.querySelector(".add_task_input");
+    todo_input.focus();
+});
+
+close_button_task.addEventListener("click", () => {
+    document.body.classList.add("hidden");
+    const alert = document.querySelector(".alert");
+    alert.classList.remove("alert_show");
+    const todo_input = document.querySelector(".add_task_input");
+    todo_input.value = "";
+});
+
+cancel_button_task.addEventListener("click", () => {
+    document.body.classList.add("hidden");
+    const alert = document.querySelector(".alert");
+    alert.classList.remove("alert_show");
+    const todo_input = document.querySelector(".add_task_input");
+    todo_input.value = "";
+});
+
+document.body.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        document.body.classList.add("hidden");
+        const todo_input = document.querySelector(".add_task_input");
+        todo_input.value = "";
+    } else if (event.key === "Enter" && document.body.classList.contains("hidden") === false) {
+        add_button_task.click();
+    }
+});
+
+add_button_task.addEventListener("click", () => {
+    const todo_input = document.querySelector(".add_task_input");
+    const todo_title = todo_input.value;
+    const task_wrapper = document.querySelector(".task_wrapper");
+    const alert = document.querySelector(".alert");
+    if (todo_title.trim() !== "") {
+        const task = document.createElement("div");
+        task.classList.add("task");
+        task.style.backgroundColor = add_task_input.style.borderColor;
+        task.innerHTML = `<div class="task_content">${todo_title}</div>
+        <button class="button single_delete_button">Delete</button>`;
+        task_wrapper.appendChild(task);
+        todo_input.value = "";
+        alert.classList.remove("alert_show");
+        document.body.classList.add("hidden");
+        const single_delete_buttons = task.querySelector(".single_delete_button");
+        single_delete_buttons.addEventListener("click", () => {
+            const task = single_delete_buttons.parentElement;
+            task.remove();
+        });
+    } else  {
+        alert.classList.add("alert_show");
+    }
+});
+
+all_delete_button.addEventListener("click", () => {
+    const task_wrapper = document.querySelector(".task_wrapper");
+    task_wrapper.innerHTML = "";
+});
